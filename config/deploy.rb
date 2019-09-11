db07f1d2abda249868e8a7279d70c55841d2a0bf
azunyan0720jp <azusa.goto@gmail.com>
2019-09-11 19:15:53 +0900

    微調整

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload credentials.yml.enc'
+  desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
