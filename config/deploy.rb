3d062010a4385a1f7bce44e80fa3e82275d023ba
azunyan0720jp <azusa.goto@gmail.com>
2019-09-11 20:18:55 +0900

    Deploy完了
    #What
    本番環境での自動デプロイ
    
    #Why
    本番環境に反映するため

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,4 +59,4 @@ set :default_env, {
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+} 
