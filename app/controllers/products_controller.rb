f6f33cfed9cf51d974274c91a297f6667d10ecaf
tech-uema <snow_clover495@yahoo.co.jp>
2019-09-14 16:35:37 +0900

    指摘点を修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new,:create,:destroy,:edit,:update]
+  before_action :authenticate_user!,      only: [:new,:create,:destroy,:edit,:update]
+  before_action :set_products_instance,   only:[:edit,:show]
+  before_action :set_products,            only:[:destroy,:update]
 
   def index
     @products = Product.includes(:images).where(status: 0).order("created_at DESC").limit(10)    #複数の指定なので返り値は配列
@@ -19,27 +21,25 @@ class ProductsController < ApplicationController
 
 
   def destroy
-    product = Product.find(params[:id])
     product.destroy if product.user_id == current_user.id
   end
 
 
 
   def edit
-    @product = Product.find(params[:id])
+
   end
 
 
 
   def show
-    @product = Product.find(params[:id])
+    
   end
 
 
 
   def update
-    product = Tweet.find(params[:id])
-    product.update(tweet_params) if product.user_id == current_user.id
+    product.update(product_params) if product.user_id == current_user.id
   end
 
 
@@ -48,6 +48,14 @@ private
 
   def product_params
     params.require(:product).permit(:name, :detail, :category, :price, :status, :state, :city, :delivery, :image).merge(user_id: current_user.id)
+  end
+
+  def set_products_instance
+    @product = Product.find(params[:id])
+  end
+
+  def set_products
+    product = Product.find(params[:id])
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations",
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   resources :products
+  resources :mypages,only: :index,
+  resources :logout,only: :index,
   root 'products#index'
+
+
 
 end
