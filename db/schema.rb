c1145de935ba4023caa69f67da1f50673384f59d
nakanishikeita03 <0816724keita@gmail.com>
2019-09-15 19:34:24 +0900

    商品出品ページの実装

--- db/schema.rb
+++ db/schema.rb
@@ -32,11 +32,6 @@ ActiveRecord::Schema.define(version: 2019_09_14_100418) do
     t.index ["product_id"], name: "index_images_on_product_id"
   end
 
-  create_table "locations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name", null: false
