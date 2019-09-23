e7f5dfe6064166a2208ab8c7824be763dda140f6
nakanishikeita03 <0816724keita@gmail.com>
2019-09-23 10:57:32 +0900

    商品出品ページ修正

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,4 +1,140 @@
-$(function(){
+$(function() {
+  var imagelabel1 = $('.o_image-label1')
+  var imagelabel2 = $('.o_image-label2')
+  var imagezone1 = $('.o_image-zone1-parent')
+  var imagezone2 = $('.o_image-zone2-parent')
+
+  // viewで表示する配列
+  var images = [];
+  // データとして送信する配列
+  var image_files = [];
+
+  // 画像アップロード
+  $(document).on('change', 'input[type= "file"]', function(){
+    var file = $(this).prop('files')[0];
+    image_files.push(file);
+    var reader = new FileReader();
+    
+    if (file.size >= 1000000) {
+      alert('1メガバイト以上の画像は登録できません。削除してください。。')
+    }
+    
+    if (file.type.match(/image/)) {
+      var img = $('<div class="add_img"><div class="img_area"><img></div></div>');
+    } else {
+      var img = $('<div class="add_img"><div class="img_area"><video controls="controls" autobuffer="true" width="110px" height="85px"></video></div></div>')
+    }
+    var btn = $('<div class="btn_wrapper"><a class="btn_delete">削除</a></div>');
+    img.append(btn);
+    
+    reader.onload = function(e) {
+      img.find('img').attr({ src: e.target.result });
+      img.find('video').attr({ src: e.target.result });
+    };
+    
+    reader.readAsDataURL(file);
+    images.push(img);
+    
+    if (images.length <= 4) {
+      $('#preview1').text('');
+      $.each(images, function(i, image){
+        image.data('image', i);
+        $('#preview1').append(image);
+      })
+      imagelabel1.css({
+        width: `calc(100% - (20% * ${images.length})`
+      })
+    } else if (images.length == 5) {
+      $('#preview1').text('');
+      $.each(images, function(i, image){
+        image.data('image', i);
+        $('#preview1').append(image);
+      })
+      imagelabel1.css({
+        display: "none"
+      })
+      imagezone2.css({
+        display: "block"
+      })
+    } else {
+      var secondrow_images = images.slice(5);
+      $.each(secondrow_images, function(i, image){
+        image.data('image', i + 5);
+        $('#preview2').append(image);
+      })
+      imagelabel2.css({
+        width: `calc(100% - (20% * ${images.length - 5})`
+      })
+    }
+    if (images.length == 10) {
+      imagelabel2.css({
+        display: "none"
+      })
+    }
+  })
+
+  // 画像削除
+  $(document).on('click', '.btn_delete', function(){
+    var target_image = $(this).parent().parent();
+    var target_image_num = target_image.data('image');
+    target_image.remove();
+    // images・image_filesから削除を押した画像を削除する
+    images.splice(target_image_num, 1);
+    image_files.splice(target_image_num, 1);
+
+    if (images.length == 0) {
+      $('input[type="file"]').data('image', 0)
+    }
+
+    if (images.length <= 4) {
+      $('#preview1').text('');
+      $.each(images, function(i, image){
+        image.data('image', i);
+        $('#preview1').append(image);
+      })
+      imagelabel1.css({
+        display: 'block',
+        width: `calc(100% - (20% * ${images.length})`
+      })
+      imagezone2.css({
+        display: 'none'
+      })
+    } else if (images.length == 5) {
+      $('#preview1').text('');
+      $.each(images, function(i, image){
+        image.data('image', i);
+        $('#preview1').append(image);
+      })
+      imagezone1.css({
+        display: 'block'
+      })
+      imagelabel2.css({
+        width: '100%'
+      })
+      imagelabel1.css({
+        display: 'none'
+      })
+      $('#preview2').text('')
+    } else {
+      var firstrow_images = images.slice(0, 5)
+      $('#preview1').text('')
+      $.each(firstrow_images, function(i, image) {
+        image.data('image', i)
+        $('#preview1').append(image)
+      })
+
+      var secondrow_images = images.slice(5)
+      $.each(secondrow_images, function(i, image){
+        image.data('image', i + 5)
+        $('#preview2').append(image)
+        imagelabel2.css({
+          display: 'block',
+          width: `calc(100% - (20% * ${images.length - 5})`
+        })
+      })
+    }
+  })
+  
 //価格表示
 $('#sell-price').on('keyup', function(){
   var price = $(this).val();
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many   :images,dependent: :destroy
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :name, presence: true
   validates :detail, presence: true
