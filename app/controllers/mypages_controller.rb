391864c7500077a8444beddff9db462f721c64a3
tech-uema <snow_clover495@yahoo.co.jp>
2019-09-24 19:04:00 +0900

    mypageコントローラーの微調整

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -9,5 +9,11 @@ class MypagesController < ApplicationController
   end
 
   def card
+    @card = Card.where(user_id: current_user.id).first
+    if @card.present?
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
   end
 end
