6c4e1ab5e2ae7b14fcc75ff4f4bd1b06dfc2f889
daiki-kato <53987124+daiki-kato@users.noreply.github.com>
2019-09-25 17:30:31 +0900

    微調整

--- app/assets/javascripts/product-image_slider.js
+++ app/assets/javascripts/product-image_slider.js
@@ -1,11 +1,6 @@
 $(function(){
   $('.image-sub').mouseover(function(){
     var selectedSrc = $(this).attr('src');
-    if (selectedSrc.match(/mp4/) || selectedSrc.match(/mov/) || selectedSrc.match(/webm/)) {
-      $('.image').addClass('display_none')
-    } else {
-      $('.image').removeClass('display_none')
-      $('.image').html(`<img src="${selectedSrc}" width="300px" height="300px">`)
-    }
+    $('.image').html(`<img src="${selectedSrc}" width="300px" height="300px">`)
   });
 });
