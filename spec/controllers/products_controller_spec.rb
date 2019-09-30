c364ed8fccc1e42ab6aaecb71d51f9582bfa9ae4
nakanishikeita03 <0816724keita@gmail.com>
2019-09-30 21:50:07 +0900

    コントローラーテスト
    #What
    コントローラーのテスト

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
-describe ProductsController do
+
+describe ProductsController, type: :controller  do
+  let(:user) {create(:user)}
+
+  describe 'GET #index' do
+  it "商品一覧ページに遷移するか" do
+    login_user user
+    get :index
+    expect(response).to render_template :index
+  end
+end
+
   describe "GET #show" do
     it "responds successfully" do
       product = create(:product)
@@ -7,4 +18,14 @@ describe ProductsController do
       expect(response).to render_template :show  
     end
   end
+
+  describe 'GET #edit' do
+  
+  it "商品編集ページに遷移するか" do
+    product = create(:product)
+    get :edit, params: { id: product }
+    expect(response).to render_template :edit
+  end
+end
+
 end
