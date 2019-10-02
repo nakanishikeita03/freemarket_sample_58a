dac431a9a14e94e6ef9e72ab21103d83045b39ce
azunyan0720jp <azusa.goto@gmail.com>
2019-10-02 20:25:21 +0900

    categoryのビュー実装
    #What
    categoryのビュー実装
    
    #Why
    categoryページを遷移できるようにするため

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 2019_09_25_091402) do
     t.index ["product_id"], name: "index_images_on_product_id"
   end
 
+  create_table "locations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "main_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
