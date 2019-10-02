89bf517ff0448c92dc9807cdc3ac7ccc05130c45
nakanishikeita03 <0816724keita@gmail.com>
2019-10-02 16:26:55 +0900

    Addressesモデルリファクタリング
    #What
    Addressモデルのバリデーション追加
    型変更
    テストコード

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :address do
-    
+    user_id               {"1"}
+    postal_code           {"111-1111"}
+    prefecture            {"1"}
+    city                  {"名古屋市"}
+    street                {"瑞穂区"}
+    building              {"ミッドランドスクエア"}
+    l_name_kanji          {"中西"}
+    f_name_kanji          {"啓太"}
+    l_name_kana           {"ナカニシ"}
+    f_name_kana           {"ケイタ"}
+    user
   end
 end
