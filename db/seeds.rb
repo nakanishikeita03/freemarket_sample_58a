07aab41ce3a196aaf1f4ccd96ca3f68c201f7d90
nakanishikeita03 <0816724keita@gmail.com>
2019-10-03 13:04:10 +0900

    微調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-require “csv”
-CSV.foreach(‘db/csv/main_data.csv’,headers: true) do |row|
- MainCategory.create!(name: row[“name”])
+require "csv"
+CSV.foreach('db/csv/main_data.csv',headers: true) do |row|
+ MainCategory.create!(name: row["name"])
 end
-CSV.foreach(‘db/csv/sub_data.csv’,headers: true) do |row|
- SubCategory.create!(name: row[“name”],main_category_id: row[“main_category_id”] )
+CSV.foreach('db/csv/sub_data.csv',headers: true) do |row|
+ SubCategory.create!(name: row["name"],main_category_id: row["main_category_id"] )
 end
-CSV.foreach(‘db/csv/sub2_data.csv’,headers: true) do |row|
- Sub2Category.create!(name: row[“name”],sub_category_id: row[“sub_category_id”])
+CSV.foreach('db/csv/sub2_data.csv',headers: true) do |row|
+ Sub2Category.create!(name: row["name"],sub_category_id: row["sub_category_id"])
 end
