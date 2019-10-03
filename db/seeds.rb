6196ee525827b1be118eeac3261f46628c255a11
nakanishikeita03 <0816724keita@gmail.com>
2019-10-03 12:42:49 +0900

    seedファイル読み込み

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require “csv”
+CSV.foreach(‘db/csv/main_data.csv’,headers: true) do |row|
+ MainCategory.create!(name: row[“name”])
+end
+CSV.foreach(‘db/csv/sub_data.csv’,headers: true) do |row|
+ SubCategory.create!(name: row[“name”],main_category_id: row[“main_category_id”] )
+end
+CSV.foreach(‘db/csv/sub2_data.csv’,headers: true) do |row|
+ Sub2Category.create!(name: row[“name”],sub_category_id: row[“sub_category_id”])
+end
