28f84fb4de9005a8d98314a8711bd4423044061d
daiki-kato <53987124+daiki-kato@users.noreply.github.com>
2019-10-04 12:36:27 +0900

    微修正
    ビュー微修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,11 +6,10 @@ class ProductsController < ApplicationController
   before_action :image_params,             only:[:update]
   
   def index
-    @products = Product.includes(:images).where(status: 0).order("created_at DESC").limit(10)    #複数の指定なので返り値は配列
-    @ladiesproducts = Product.includes(:images).where(category: 1..180).order("created_at DESC").limit(10)
-    @mensproducts = Product.includes(:images).where(category: 181..310).order("created_at DESC").limit(10)
-    @appliancesproducts = Product.includes(:images).where(category: 799..871).order("created_at DESC").limit(10)
-    @toysproducts = Product.includes(:images).where(category: 612..669).order("created_at DESC").limit(10)
+    @ladiesproducts = Product.includes(:images).where(status: 0).where(category: 1..180).order("created_at DESC").limit(10)
+    @mensproducts = Product.includes(:images).where(status: 0).where(category: 181..310).order("created_at DESC").limit(10)
+    @appliancesproducts = Product.includes(:images).where(status: 0).where(category: 799..871).order("created_at DESC").limit(10)
+    @toysproducts = Product.includes(:images).where(status: 0).where(category: 612..669).order("created_at DESC").limit(10)
     # @category = MainCategory.all.includes(sub_categories: :sub2_categories)
   end
 
